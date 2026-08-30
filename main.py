d44d6e7a3bffadfd09871e3635532cc26fb9126b
crescent-nur <akoredenurudeen33@gmail.com>
2026-08-30 01:17:31 +0100

    Add FastAPI server with working homepage

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
 from app.database import Base, engine
 from app.models import models
 
 Base.metadata.create_all(bind=engine)
-print("Tables created:", list(Base.metadata.tables.keys()))
+
+app = FastAPI(title="DocuVerify")
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
+
+
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse(request, "index.html")
